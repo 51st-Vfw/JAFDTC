a3b47714d004b1b3a65a1076efaece9a8ed643f5
ilominar <85649993+ilominar@users.noreply.github.com>
2025-10-17 18:25:34 -0700

    Update EditWaypointListHelperBase.cs
    Added GPL header.

--- JAFDTC/UI/Base/EditWaypointListHelperBase.cs
+++ JAFDTC/UI/Base/EditWaypointListHelperBase.cs
@@ -1,5 +1,23 @@
-﻿using JAFDTC.Models;
-using JAFDTC.Models.A10C;
+﻿// ********************************************************************************************************************
+//
+// EditWaypointListHelperBase.cs : base class for EditNavPointListPage helper classes
+//
+// Copyright(C) 2023-2025 fizzle
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.  If not, see
+// <https://www.gnu.org/licenses/>.
+//
+// ********************************************************************************************************************
+
+using JAFDTC.Models;
 using JAFDTC.Models.Base;
 using JAFDTC.Models.DCS;
 using JAFDTC.UI.Controls.Map;
@@ -9,9 +27,6 @@ using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JAFDTC.UI.Base
 {
