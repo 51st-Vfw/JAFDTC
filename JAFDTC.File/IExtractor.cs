ffbe2bdd648f30f03b0f79c83fefac824652eb61
ilominar <85649993+ilominar@users.noreply.github.com>
2025-12-15 22:43:57 -0800

    Update File Headers
    Added file headers in JAFDTC.File.Models files.

--- JAFDTC.File/IExtractor.cs
+++ JAFDTC.File/IExtractor.cs
@@ -1,4 +1,23 @@
-﻿using JAFDTC.File.Models;
+﻿// ********************************************************************************************************************
+//
+// IExtractor.cs -- jafdtc file extractor interface
+//
+// Copyright(C) 2025 rage
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.  If not, see
+// <https://www.gnu.org/licenses/>.
+//
+// ********************************************************************************************************************
+
+using JAFDTC.File.Models;
 using JAFDTC.Models.Units;
 
 namespace JAFDTC.File
--- JAFDTC.File/Models/ExtractCriteria.cs
+++ JAFDTC.File/Models/ExtractCriteria.cs
@@ -1,4 +1,23 @@
-﻿using JAFDTC.Models.Core;
+﻿// ********************************************************************************************************************
+//
+// ExtractCriteria.cs -- jafdtc model object extraction criteria for use in IExtractor
+//
+// Copyright(C) 2025 rage
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.  If not, see
+// <https://www.gnu.org/licenses/>.
+//
+// ********************************************************************************************************************
+
+using JAFDTC.Models.Core;
 using JAFDTC.Models.Units;
 
 namespace JAFDTC.File.Models
@@ -6,7 +25,7 @@ namespace JAFDTC.File.Models
     public class ExtractCriteria
     {
         public required string FilePath { get; set; }
-        public string Theater { get; set; }
+        public string? Theater { get; set; }
         public CoalitionType[]? Coalitions { get; set; }
         public UnitCategoryType[]? UnitCategories { get; set; }
         public string[]? UnitTypes { get; set; } // TODO: move this to an enum?
