9e1aef3cda5fe271c5f4c1425d27e5d45933f793
ilominar <85649993+ilominar@users.noreply.github.com>
2025-12-29 20:25:55 -0800

    Update ConfigurationListPage.xaml.cs
    Added Rage to aboot box.

--- JAFDTC/UI/App/ConfigurationListPage.xaml.cs
+++ JAFDTC/UI/App/ConfigurationListPage.xaml.cs
@@ -709,7 +709,7 @@ namespace JAFDTC.UI.App
                              $"\n" +
                              $"On borrowed time until ED releases their native solution (2 weeks!)...\n" +
                              $"\n" +
-                             $"A Raven & fizzle / 51st VFW fork";
+                             $"A Raven & fizzle & Rage / 51st VFW fork";
             await Utilities.Message1BDialog(Content.XamlRoot, $"JAFDTC {Settings.VersionJAFDTC}", content);
         }
 
