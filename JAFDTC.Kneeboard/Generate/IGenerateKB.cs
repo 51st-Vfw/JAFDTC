85f7fc52a7ff375b26d9b794680a3dfccf496980
PCH 07 <pch07@hotmail.com>
2025-12-31 09:33:53 -0800

    merge n clean

--- JAFDTC.Kneeboard/Generate/IGenerateKB.cs
+++
@@ -1,9 +0,0 @@
-﻿using JAFDTC.Kneeboard.Models;
-
-namespace JAFDTC.Kneeboard.Generate
-{
-    internal interface IGenerateKB : IDisposable
-    {
-        string Process(GenerateCriteria generateCriteria, string templateFilePath);
-    }
-}
