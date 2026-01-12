925bf0923feb3477f9cd507cad20c144be220ee2
ilominar <85649993+ilominar@users.noreply.github.com>
2026-01-11 16:09:17 -0800

    Create Glyphs.cs
    Consolidating common glyph definitions in single file for use in code. Should make things a  tad more readable.

---
+++ JAFDTC/UI/Glyphs.cs
@@ -0,0 +1,38 @@
+﻿// ********************************************************************************************************************
+//
+// Glyphs.cs : ui constatns for glyphs
+//
+// Copyright(C) 2026 ilominar/raven
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.  If not, see
+// <https://www.gnu.org/licenses/>.
+//
+// ********************************************************************************************************************
+
+namespace JAFDTC.UI
+{
+    public class Glyphs
+    {
+        public const string None = "";
+        public const string Pilot = "\xE77B";                   // single person
+        public const string Pilots = "\xE716";                  // multiple people
+
+        public const string Countermeasures = "\xEA18";         // countermeasure, defensive system
+        public const string Displays = "\xE950";                // display, mfd systems
+        public const string DTC = "\xE77C";                     // core data transfer cartridge system
+        public const string Kneeboard = "\xF0E3";               // core kneeboard system
+        public const string Miscellaneous = "\xE8B7";           // miscellaneous system
+        public const string Mission = "\xE74C";                 // core mission system
+        public const string Munitions = "\xEBD2";               // munitions system
+        public const string Navigation = "\xE707";              // navigation system
+        public const string Radio = "\xE704";                   // radio, communications system
+    }
+}
