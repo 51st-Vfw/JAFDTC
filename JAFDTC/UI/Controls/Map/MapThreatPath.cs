68a3ea496687545bb8fc0a467baa51e3d0ad3c99
ilominar <85649993+ilominar@users.noreply.github.com>
2026-01-22 20:05:33 -0800

    Update MapThreatPath.cs
    At high zooms, Windows gets cranky and stops displaying threat rings. Added hack to clip the threat ring to the screen (like the hack in the route code to keep the routes from disappearing at high zoom). It's too bad this is a thing, because it is a pain.

--- JAFDTC/UI/Controls/Map/MapThreatPath.cs
+++ JAFDTC/UI/Controls/Map/MapThreatPath.cs
@@ -2,7 +2,7 @@
 //
 // MapThreatPath.cs : path geometry for a threat ring in map window
 //
-// Copyright(C) 2025 ilominar/raven
+// Copyright(C) 2025-2026 ilominar/raven
 //
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
 // Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
@@ -64,6 +64,13 @@ namespace JAFDTC.UI.Controls.Map
         //
         // ------------------------------------------------------------------------------------------------------------
 
+        // (again) don't EVEN think about asking, dude...
+        //
+        private const double CLIP_HACK_RMAG = 8192.0;
+
+        private const double CLIP_OFFSET = 4.0;
+        private const double EPSILON = 1.0e-6;
+
         private const double METER_PER_NAUTMILE = 1852.0;
         private const double EARTH_RADIUS = 6378137.0;
 
@@ -106,7 +113,7 @@ namespace JAFDTC.UI.Controls.Map
         /// in nautical miles. locations are the center and the edge of the wez due east from center.
         /// </summary>
         public static List<Location> MakeLocationsForThreat(Location center, double wezRadius)
-            => [ center, center.GetLocation(90.0 * (Math.PI / 180.0), (wezRadius * METER_PER_NAUTMILE) / EARTH_RADIUS) ];
+            => [center, center.GetLocation(90.0 * (Math.PI / 180.0), (wezRadius * METER_PER_NAUTMILE) / EARTH_RADIUS)];
 
         // ------------------------------------------------------------------------------------------------------------
         //
@@ -150,7 +157,8 @@ namespace JAFDTC.UI.Controls.Map
         }
 
         /// <summary>
-        /// add arcs to the geometry for the threat ring.
+        /// add arcs to the geometry for the threat ring. locations should contain two points: the first is the
+        /// center, and the second is a point on the ring.
         /// </summary>
         private void AddThreatRing(PathFigureCollection figures, IEnumerable<Location> locations, double lonOffset)
         {
@@ -173,75 +181,215 @@ namespace JAFDTC.UI.Controls.Map
                     // HACK WARNING: windows gets cranky when the geometry gets really big (for exmaple, when
                     // HACK WARNING: zoomed in). this can cause the gemetry to just disappear at high zoom
                     // HACK WARNING: levels. figure out if we are so zoomed in the entire visible map is under
-                    // HACK WARNING: the threat ring. if so, we'll change the geometry so it just covers the
-                    // HACK WARNING: visible map.
+                    // HACK WARNING: the threat ring based on the wez radius. if so, we'll draw the threat ring
+                    // HACK WARNING: as the intersection of a rect and circle, rather than just a circle.
                     //
-                    // set up edgeWEZ and centerWEZ for the geometry.
-                    //
-                    double distDiag = Math.Sqrt(Math.Pow(ParentMap.ActualWidth, 2.0) +
-                                                Math.Pow(ParentMap.ActualHeight, 2.0));
-                    double distCenters = Math.Sqrt(Math.Pow((0.5 * ParentMap.ActualWidth) - center.X, 2.0) +
-                                                   Math.Pow((0.5 * ParentMap.ActualHeight) - center.Y, 2.0));
-                    Point edgeWEZ;
-                    Point centerWEZ;
-                    if (radius > (distCenters + distDiag))
+                    if (radius > CLIP_HACK_RMAG)
                     {
-                        radius = distDiag * 0.60;
-                        centerWEZ = new Point(ParentMap.ActualWidth / 2.0, ParentMap.ActualHeight / 2.0);
-                        edgeWEZ = new Point(centerWEZ.X + radius, centerWEZ.Y);
+                        List<Point> intersect = FindIntersections(ParentMap.ActualWidth, ParentMap.ActualHeight,
+                                                                  center, radius);
+                        if (intersect.Count > 0)
+                            figures.Add(ClippedCirclePath(center, radius, intersect));
                     }
                     else
                     {
-                        centerWEZ = center;
-                        edgeWEZ = edge;
+                        figures.Add(CirclePath(edge, radius));
                     }
-
-// TODO: this is not quite right. we arc between the east and west points on the circle. when either of these
-// TODO: points gets "far enough" from the visible area, the gemetry gets dropped. the above code handles the
-// TODO: case where the visible area is entirely contained within the circle (and the circle is large). it
-// TODO: does not handle the case where the circle passes through the visible area (and the circle is large).
-// TODO: in this case, we really need to clip the circle to the visible area. this can only produce one segment
-// TODO: crossing the visible area (if there are multiple segments, the circle must be small enough to not trip
-// TODO: the "circle is large" constraint).
-// TODO:
-// TODO: what we probably need to do is the following:
-// TODO:
-// TODO: (1) determine distances from view center to east/west ref points.
-// TODO: (2) if any distance >= critical value and view area lies entirely within circle,
-// TODO:     --> reduce circle size so border is just outside view area 
-// TODO: (3) if any distance >= critical value and view area crosses circle,
-// TODO:     --> clip circle to view area and draw segment of circle lying between view area edges
-// TODO: (4) if all distances < critical value,
-// TODO:     --> draw as normal
-
-                    // build out the geometry. note that the center, edge, and radius may have been changed by the
-                    // code above if the wez is "too big".
-                    //
-                    PathFigure figure = new()
-                    {
-                        StartPoint = edgeWEZ,
-                        IsClosed = true,
-                    };
-
-                    ArcSegment arcBottom = new()
-                    {
-                        Point = new Windows.Foundation.Point(edgeWEZ.X - (2.0 * radius), edgeWEZ.Y),
-                        Size = new Windows.Foundation.Size(radius, radius),
-                        IsLargeArc = true,
-                        SweepDirection = SweepDirection.Clockwise
-                    };
-                    figure.Segments.Add(arcBottom);
-                    ArcSegment arcTop = new()
-                    {
-                        Point = edgeWEZ,
-                        Size = new Windows.Foundation.Size(radius, radius),
-                        IsLargeArc = true,
-                        SweepDirection = SweepDirection.Clockwise
-                    };
-                    figure.Segments.Add(arcTop);
-                    figures.Add(figure);
                 }
             }
+        }
+
+        /// <summary>
+        /// return a path for a circle of radius r that passes through the point due east of the center at e.
+        /// </summary>
+        private static PathFigure CirclePath(Point e, double r)
+        {
+            PathFigure figure = new()
+            {
+                StartPoint = e,
+                IsClosed = true,
+            };
+
+            ArcSegment arcBottom = new()
+            {
+                Point = new Windows.Foundation.Point(e.X - (2.0 * r), e.Y),
+                Size = new Windows.Foundation.Size(r, r),
+                IsLargeArc = true,
+                SweepDirection = SweepDirection.Clockwise
+            };
+            figure.Segments.Add(arcBottom);
+            ArcSegment arcTop = new()
+            {
+                Point = e,
+                Size = new Windows.Foundation.Size(r, r),
+                IsLargeArc = true,
+                SweepDirection = SweepDirection.Clockwise
+            };
+            figure.Segments.Add(arcTop);
+            return figure;
+        }
+
+        /// <summary>
+        /// return a path for a circle of radius r and center c intersected with a rectangle. the points list are
+        /// cw ordered intersection points between the circle and rectangle.
+        /// </summary>
+        private static PathFigure ClippedCirclePath(Point c, double r, List<Point> points)
+        {
+            PathFigure figure = new()
+            {
+                StartPoint = points[0],
+                IsClosed = true,
+            };
+
+            for (int i = 0; i < (points.Count - 1); i++)
+            {
+                Point cur = points[i];
+                Point nxt = points[(i + 1) % points.Count];         // wrap around
+
+                // Check if both points are on the circle's edge (within tolerance)
+                bool isCurOnCircle = (Math.Abs(Distance(cur, c) - r) < EPSILON);
+                bool isNxtOnCircle = (Math.Abs(Distance(nxt, c) - r) < EPSILON);
+
+                if (isCurOnCircle && isNxtOnCircle)
+                    figure.Segments.Add(new ArcSegment()
+                    {
+                        Point = nxt,
+                        Size = new Windows.Foundation.Size(r, r),
+                        IsLargeArc = false,
+                        SweepDirection = SweepDirection.Clockwise
+                    });
+                else
+                    figure.Segments.Add(new LineSegment() { Point = nxt });
+            }
+            return figure;
+        }
+
+        /// <summary>
+        /// returns distance between two points.
+        /// </summary>
+        private static double Distance(Point a, Point b)
+        {
+            double dx = a.X - b.X;
+            double dy = a.Y - b.Y;
+            return Math.Sqrt((dx * dx) + (dy * dy));
+        }
+
+        /// <summary>
+        /// return intersection points for a rectangle and circle. rectangle origin is at (0, 0) and is slightly
+        /// expanded.
+        /// </summary>
+        private static List<Point> FindIntersections(double w, double h, Point c, double r)
+        {
+            // Rectangle bounds
+            double xmin = -CLIP_OFFSET, xmax = w + CLIP_OFFSET;
+            double ymin = -CLIP_OFFSET, ymax = h + CLIP_OFFSET;
+
+            List<Point> points = [];
+
+            // 1. Add rectangle corners inside circle
+            AddIfInsideCircle(new Point(xmin, ymin), c, r, points);
+            AddIfInsideCircle(new Point(xmax, ymin), c, r, points);
+            AddIfInsideCircle(new Point(xmax, ymax), c, r, points);
+            AddIfInsideCircle(new Point(xmin, ymax), c, r, points);
+
+            // 2. Check each rectangle edge for intersection with circle
+            AddLineCircleIntersections(xmin, ymin, xmax, ymin, c, r, points); // Bottom
+            AddLineCircleIntersections(xmin, ymax, xmax, ymax, c, r, points); // Top
+            AddLineCircleIntersections(xmin, ymin, xmin, ymax, c, r, points); // Left
+            AddLineCircleIntersections(xmax, ymin, xmax, ymax, c, r, points); // Right
+
+            // 3. Remove duplicates (within tolerance) and order points into polygon
+            return OrderPoints(RemoveDuplicates(points, EPSILON));
+        }
+
+        /// <summary>
+        /// adds a point to a list of points if it falls within a circle.
+        /// </summary>
+        private static void AddIfInsideCircle(Point p, Point c, double r, List<Point> list)
+        {
+            if (((p.X - c.X) * (p.X - c.X)) + ((p.Y - c.Y) * (p.Y - c.Y)) <= (r * r))
+                list.Add(p);
+        }
+
+        /// <summary>
+        /// TODO: document
+        /// </summary>
+        private static void AddLineCircleIntersections(double x1, double y1, double x2, double y2,
+                                                       Point c, double r, List<Point> list)
+        {
+            double dx = x2 - x1;
+            double dy = y2 - y1;
+
+            double fx = x1 - c.X;
+            double fy = y1 - c.Y;
+
+            double qa = (dx * dx) + (dy * dy);
+            double qb = 2 * ((fx * dx) + (fy * dy));
+            double qc = (fx * fx) + (fy * fy) - r * r;
+
+            double discriminant = (qb * qb) - (4 * qa * qc);
+            if (discriminant >= 0.0)
+            {
+                discriminant = Math.Sqrt(discriminant);
+
+                double t1 = (-qb - discriminant) / (2.0 * qa);
+                double t2 = (-qb + discriminant) / (2.0 * qa);
+
+                if ((t1 >= 0.0) && (t1 <= 1.0))
+                    list.Add(new Point(x1 + (t1 * dx), y1 + (t1 * dy)));
+                if ((t2 >= 0.0) && (t2 <= 1.0) && (discriminant > 0.0))
+                    list.Add(new Point(x1 + (t2 * dx), y1 + (t2 * dy)));
+            }
+        }
+
+        /// <summary>
+        /// remove duplicates from a list of points. to be considered a duplicate, a point must be within a
+        /// tolerance of another point in x and y.
+        /// </summary>
+        private static List<Point> RemoveDuplicates(List<Point> points, double tolerance)
+        {
+            List<Point> unique = [ ];
+            foreach (var p in points)
+            {
+                bool exists = false;
+                foreach (var q in unique)
+                    if ((Math.Abs(p.X - q.X) < tolerance) && (Math.Abs(p.Y - q.Y) < tolerance))
+                    {
+                        exists = true;
+                        break;
+                    }
+                if (!exists)
+                    unique.Add(p);
+            }
+            return unique;
+        }
+
+        /// <summary>
+        /// order a list of points by polar angle around their centroid, points are ordered in cw order.
+        /// </summary>
+        private static List<Point> OrderPoints(List<Point> points)
+        {
+            if (points.Count > 1)
+            {
+                // 1. Find centroid
+                double cx = 0, cy = 0;
+                foreach (Point p in points)
+                {
+                    cx += p.X;
+                    cy += p.Y;
+                }
+                cx /= points.Count;
+                cy /= points.Count;
+
+                // 2. Sort by polar angle around centroid
+                points.Sort((a, b) =>
+                {
+                    double angleA = Math.Atan2(a.Y - cy, a.X - cx);
+                    double angleB = Math.Atan2(b.Y - cy, b.X - cx);
+                    return angleA.CompareTo(angleB);
+                });
+            }
+            return points;
         }
     }
 }
